94d9a0f90251daee13f76c87679537041168107d
Laura <laurahannah44@gmail.com>
2023-06-04 16:53:40 +0100

    trying to get the replacement font working

--- _site/scripts and filters/scripts.js
+++ _site/scripts and filters/scripts.js
@@ -1,15 +1,23 @@
 // script.js
 window.addEventListener('DOMContentLoaded', () => {
-    const contents = document.querySelectorAll('.content');
+  const contents = document.querySelectorAll('.content');
   
-    contents.forEach(content => {
-      const textboxes = content.querySelectorAll('.textbox');
+  contents.forEach(content => {
+    const textboxes = content.querySelectorAll('.textbox');
   
-      let maxHeight = 0;
-      textboxes.forEach(textbox => {
-        const height = textbox.offsetTop + textbox.offsetHeight;
-        maxHeight = Math.max(maxHeight, height);
-      });
-      content.style.height = maxHeight + 'px';
+    let maxHeight = 0;
+    textboxes.forEach(textbox => {
+      const height = textbox.offsetTop + textbox.offsetHeight;
+      maxHeight = Math.max(maxHeight, height);
     });
+    content.style.height = maxHeight + 'px';
   });
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  var scaler = document.querySelector('.scaler');
+  var scalerHeight = scaler.offsetHeight * 4;
+  var scalerWidth = scaler.offsetWidth * 4;
+  document.body.style.height = scalerHeight + 'px';
+  document.body.style.width = scalerWidth + 'px';
+});
