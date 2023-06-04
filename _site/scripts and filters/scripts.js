915f8b4a88004ff1fdaef78614d83371df06362b
Laura <laurahannah44@gmail.com>
2023-06-04 23:52:55 +0100

    working on a script to adjust center-aligned text lines based on odd parity

--- _site/scripts and filters/scripts.js
+++ _site/scripts and filters/scripts.js
@@ -14,10 +14,75 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
   var scaler = document.querySelector('.scaler');
   var scalerHeight = scaler.offsetHeight * 4;
   var scalerWidth = scaler.offsetWidth * 4;
   document.body.style.height = scalerHeight + 'px';
   document.body.style.width = scalerWidth + 'px';
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Get the text box elements
+  const textboxes = document.querySelectorAll('.textbox');
+
+  textboxes.forEach(textbox => {
+
+    // Get the computed style of the text box
+    const computedStyle = window.getComputedStyle(textbox);
+
+    // Get the width of the text box
+    const textboxWidth = parseInt(computedStyle.width);
+
+    // Split the text content into an array of lines
+    const textContent  = textbox.textContent;
+
+    const tempElement = document.createElement('span');
+    document.body.appendChild(tempElement)
+    tempElement.style.whiteSpace = 'nowrap';
+    tempElement.style.visibility = 'hidden';
+    tempElement.style.fontFamily = computedStyle.fontFamily;
+    tempElement.style.fontSize = computedStyle.fontSize;
+
+    // Split the text content into words
+    const words = textContent.split(' ');
+
+    // Initialize variables
+    let currentLine = '';
+    const lines = [];
+
+    // Loop through each word
+    for (const word of words) {
+      console.log(`Word: ${word}`);
+      // Add the word to the current line
+      const testLine = currentLine ? currentLine + ' ' + word : word;
+
+      // Set the text content of the temporary element to the test line
+      tempElement.textContent = testLine;
+
+      console.log(textboxWidth, tempElement.offsetWidth, testLine)
+      // Check if the test line exceeds the width of the text box
+      if (tempElement.offsetWidth > textboxWidth) {
+
+        // Add the current line to the lines array
+        lines.push(currentLine);
+
+        // Start a new line with the current word
+        currentLine = word;
+      } else {
+        // Update the current line
+        currentLine = testLine;
+      }
+    }
+
+    // Add the last line to the lines array
+    lines.push(currentLine);
+
+    // Iterate through each line and perform operations
+    lines.forEach((line, index) => {
+
+      // Perform desired operations on each line
+      console.log(`Line ${index + 1}: ${line}`);
+    });
+  });
 });
