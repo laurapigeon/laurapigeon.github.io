0320d322c716e7cb687443f2b5bec383891d801e
Laura <laurahannah44@gmail.com>
2023-06-05 00:59:48 +0100

    finished font center alignment~ hyperlinks next

--- scripts and filters/scripts.js
+++ scripts and filters/scripts.js
@@ -30,59 +30,72 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Get the computed style of the text box
     const computedStyle = window.getComputedStyle(textbox);
+    
+    if (computedStyle.textAlign === 'center') {
 
-    // Get the width of the text box
-    const textboxWidth = parseInt(computedStyle.width);
-
-    // Split the text content into an array of lines
-    const textContent  = textbox.textContent;
-
-    const tempElement = document.createElement('span');
-    document.body.appendChild(tempElement)
-    tempElement.style.whiteSpace = 'nowrap';
-    tempElement.style.visibility = 'hidden';
-    tempElement.style.fontFamily = computedStyle.fontFamily;
-    tempElement.style.fontSize = computedStyle.fontSize;
-
-    // Split the text content into words
-    const words = textContent.split(' ');
-
-    // Initialize variables
-    let currentLine = '';
-    const lines = [];
-
-    // Loop through each word
-    for (const word of words) {
-      console.log(`Word: ${word}`);
-      // Add the word to the current line
-      const testLine = currentLine ? currentLine + ' ' + word : word;
-
-      // Set the text content of the temporary element to the test line
-      tempElement.textContent = testLine;
-
-      console.log(textboxWidth, tempElement.offsetWidth, testLine)
-      // Check if the test line exceeds the width of the text box
+      // Get the width of the text box
+      const textboxWidth = parseInt(computedStyle.width);
+      
+      // Split the text content into an array of lines
+      const textContent  = textbox.textContent;
+      
+      const tempElement = document.createElement('span');
+      document.body.appendChild(tempElement)
+      tempElement.style.whiteSpace = 'nowrap';
+      tempElement.style.visibility = 'hidden';
+      tempElement.style.fontFamily = computedStyle.fontFamily;
+      tempElement.style.fontSize = computedStyle.fontSize;
+      
+      // Split the text content into words
+      const words = textContent.split(' ');
+      
+      // Initialize variables
+      let currentLine = '';
+      const lines = [];
+      
+      // Loop through each word
+      for (const word of words) {
+        // Add the word to the current line
+        const testLine = currentLine ? currentLine + ' ' + word : word;
+        
+        // Set the text content of the temporary element to the test line
+        tempElement.textContent = testLine;
+        
+        // Check if the test line exceeds the width of the text box
       if (tempElement.offsetWidth > textboxWidth) {
 
         // Add the current line to the lines array
         lines.push(currentLine);
-
+        
         // Start a new line with the current word
         currentLine = word;
       } else {
         // Update the current line
         currentLine = testLine;
-      }
+        }
+      } 
+    
+      // Add the last line to the lines array
+      lines.push(currentLine);
+      
+      textbox.textContent = '';
+      
+      // Iterate through each line and perform operations
+      lines.forEach((line, index) => {
+        
+        tempElement.textContent = line
+        
+        // Perform desired operations on each line
+        console.log(`Line ${index + 1}: ${line}`, tempElement.offsetWidth, tempElement.offsetWidth % 2 == 0 ? `(onset)` : `(offset)`);
+        
+        const linebox = document.createElement('div');
+        linebox.classList.add('.linebox');
+        linebox.textContent = line;
+        if (tempElement.offsetWidth % 2 == 1) {
+          linebox.style.transform = 'translateX(-0.5px)';
+        }
+        textbox.appendChild(linebox);
+      });
     }
-
-    // Add the last line to the lines array
-    lines.push(currentLine);
-
-    // Iterate through each line and perform operations
-    lines.forEach((line, index) => {
-
-      // Perform desired operations on each line
-      console.log(`Line ${index + 1}: ${line}`);
-    });
   });
 });
