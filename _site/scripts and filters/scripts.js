c3145afb84d9eb476980fca7d7b069950aa2a12a
Laura <laurahannah44@gmail.com>
2023-06-05 16:13:10 +0100

    fixed load order shenanagins, added scaler container

--- _site/scripts and filters/scripts.js
+++ _site/scripts and filters/scripts.js
@@ -1,10 +1,3 @@
-var scaler = document.querySelector('.scaler');
-var scalerHeight = scaler.offsetHeight * 4;
-var scalerWidth = scaler.offsetWidth * 4;
-document.body.style.height = scalerHeight + 'px';
-document.body.style.width = scalerWidth + 'px';
-
-
 window.addEventListener('load', function() {
   // Create temporary element to test line lengths
   const tempElement = document.createElement('span');
@@ -84,17 +77,25 @@ window.addEventListener('load', function() {
   });
   //tempElement.textContent = 'Lorem ipsum dolor sit amet,';
   //console.log(`Line: Lorem ipsum dolor sit amet, len ${tempElement.offsetWidth}`);
-});
+  
+  document.body.removeChild(tempElement)
 
-const contents = document.querySelectorAll('.content');
-
-contents.forEach(content => {
-  const textboxes = content.querySelectorAll('.textbox');
-
-  let maxHeight = 0;
-  textboxes.forEach(textbox => {
-    const height = textbox.offsetTop + textbox.offsetHeight;
-    maxHeight = Math.max(maxHeight, height);
+  // Calculate the new height of the content boxes based on the text boxes within
+  const contents = document.querySelectorAll('.content');
+  
+  contents.forEach(content => {
+    const textboxes = content.querySelectorAll('.textbox');
+    
+    let maxHeight = 0;
+    textboxes.forEach(textbox => {
+      const height = textbox.offsetTop + textbox.offsetHeight;
+      maxHeight = Math.max(maxHeight, height);
+    });
+    content.style.height = maxHeight + 'px';
   });
-  content.style.height = maxHeight + 'px';
+
+  // Calculate the dimensions of the scaler container based on the scalers height
+  var scaler = document.querySelector('.scaler');
+  scaler.parentElement.style.height = (scaler.offsetHeight * 4) + 'px';
+  scaler.parentElement.style.width = (scaler.offsetWidth * 4) + 'px';
 });
