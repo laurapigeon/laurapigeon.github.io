663ea0b3c9cbf56ab92da34bdf06765a124de158
Laura <laurahannah44@gmail.com>
2023-06-05 14:56:07 +0100

    need to address load order issues

--- _site/scripts and filters/scripts.js
+++ _site/scripts and filters/scripts.js
@@ -1,102 +1,100 @@
-// script.js
-window.addEventListener('DOMContentLoaded', () => {
-  const contents = document.querySelectorAll('.content');
-  
-  contents.forEach(content => {
-    const textboxes = content.querySelectorAll('.textbox');
-  
-    let maxHeight = 0;
-    textboxes.forEach(textbox => {
-      const height = textbox.offsetTop + textbox.offsetHeight;
-      maxHeight = Math.max(maxHeight, height);
-    });
-    content.style.height = maxHeight + 'px';
-  });
-});
+var scaler = document.querySelector('.scaler');
+var scalerHeight = scaler.offsetHeight * 4;
+var scalerWidth = scaler.offsetWidth * 4;
+document.body.style.height = scalerHeight + 'px';
+document.body.style.width = scalerWidth + 'px';
 
-window.addEventListener('DOMContentLoaded', () => {
-  var scaler = document.querySelector('.scaler');
-  var scalerHeight = scaler.offsetHeight * 4;
-  var scalerWidth = scaler.offsetWidth * 4;
-  document.body.style.height = scalerHeight + 'px';
-  document.body.style.width = scalerWidth + 'px';
-});
 
-window.addEventListener('DOMContentLoaded', () => {
-  // Get the text box elements
+window.addEventListener('load', function() {
+  // Create temporary element to test line lengths
+  const tempElement = document.createElement('span');
+  tempElement.style.whiteSpace = 'nowrap';
+  tempElement.style.visibility = 'hidden';
+
+  // Get all textbox elements
   const textboxes = document.querySelectorAll('.textbox');
 
+  // Loop through textboxes replacing text contents with lineboxes
   textboxes.forEach(textbox => {
 
-    // Get the computed style of the text box
+    // Get the computed style of the textbox
     const computedStyle = window.getComputedStyle(textbox);
     
-    if (computedStyle.textAlign === 'center') {
+    // Make temp elem match textbox stylings
+    tempElement.style.fontFamily = computedStyle.fontFamily;
+    tempElement.style.fontSize = computedStyle.fontSize;
+    document.body.appendChild(tempElement)
+    
+    // Get the width of the textbox
+    const textboxWidth = parseInt(computedStyle.width);
 
-      // Get the width of the text box
-      const textboxWidth = parseInt(computedStyle.width);
+    // Split the text content into words
+    const words = textbox.textContent.split(' ');
+    
+    // Initialize variables
+    let currentLine = '';
+    const lines = [];
+    
+    // Loop through each word
+    for (const word of words) {
+      // Add the word to the current line
+      const testLine = currentLine ? currentLine + ' ' + word : word;
       
-      // Split the text content into an array of lines
-      const textContent  = textbox.textContent;
+      // Set the text content of the temporary element to the test line
+      tempElement.textContent = testLine;
       
-      const tempElement = document.createElement('span');
-      document.body.appendChild(tempElement)
-      tempElement.style.whiteSpace = 'nowrap';
-      tempElement.style.visibility = 'hidden';
-      tempElement.style.fontFamily = computedStyle.fontFamily;
-      tempElement.style.fontSize = computedStyle.fontSize;
-      
-      // Split the text content into words
-      const words = textContent.split(' ');
-      
-      // Initialize variables
-      let currentLine = '';
-      const lines = [];
-      
-      // Loop through each word
-      for (const word of words) {
-        // Add the word to the current line
-        const testLine = currentLine ? currentLine + ' ' + word : word;
-        
-        // Set the text content of the temporary element to the test line
-        tempElement.textContent = testLine;
-        
-        // Check if the test line exceeds the width of the text box
+      // Check if the test line exceeds the width of the text box
       if (tempElement.offsetWidth > textboxWidth) {
-
+        
         // Add the current line to the lines array
         lines.push(currentLine);
+        
+        //console.log(`Line: ${testLine} len ${tempElement.offsetWidth} longer than ${textboxWidth}`);
         
         // Start a new line with the current word
         currentLine = word;
       } else {
         // Update the current line
         currentLine = testLine;
-        }
-      } 
+      }
+    } 
     
-      // Add the last line to the lines array
-      lines.push(currentLine);
+    // Add the last line to the lines array
+    lines.push(currentLine);
+    
+    textbox.textContent = '';
+    
+    // Iterate through each line and perform operations
+    lines.forEach((line, index) => {
       
-      textbox.textContent = '';
+      tempElement.textContent = line
       
-      // Iterate through each line and perform operations
-      lines.forEach((line, index) => {
-        
-        tempElement.textContent = line
-        
-        // Perform desired operations on each line
-        //console.log(`Line ${index + 1}: ${line}`, tempElement.offsetWidth, tempElement.offsetWidth % 2 == 0 ? `(onset)` : `(offset)`);
-        
-        const linebox = document.createElement('div');
-        linebox.classList.add('.linebox');
-        linebox.textContent = line;
-        if (tempElement.offsetWidth % 2 == 1) {
-          linebox.style.transform = 'translateX(-0.5px)';
-        }
-        textbox.appendChild(linebox);
-      });
+      // Perform desired operations on each line
+      //console.log(`Line ${index + 1}: ${line}`, tempElement.offsetWidth, tempElement.offsetWidth % 2 == 0 ? `(onset)` : `(offset)`);
+      
+      const linebox = document.createElement('div');
+      linebox.classList.add('.linebox');
+      linebox.textContent = line;
+      if (computedStyle.textAlign === 'center' && tempElement.offsetWidth % 2 == 1) {
+        linebox.style.transform = 'translateX(-0.5px)';
+      }
+      textbox.appendChild(linebox);
+    });
       //console.log(`colour was ${computedStyle.color}`)
-    }
   });
+  //tempElement.textContent = 'Lorem ipsum dolor sit amet,';
+  //console.log(`Line: Lorem ipsum dolor sit amet, len ${tempElement.offsetWidth}`);
+});
+
+const contents = document.querySelectorAll('.content');
+
+contents.forEach(content => {
+  const textboxes = content.querySelectorAll('.textbox');
+
+  let maxHeight = 0;
+  textboxes.forEach(textbox => {
+    const height = textbox.offsetTop + textbox.offsetHeight;
+    maxHeight = Math.max(maxHeight, height);
+  });
+  content.style.height = maxHeight + 'px';
 });
