21ca191c921003e7ddf671bcf0e36ee1cd4f8898
laurapigeon <laurahannah44@gmail.com>
2024-08-25 02:22:54 +0100

    upkeep, button disablage

---
+++ scripts/disable-buttons.js
@@ -0,0 +1,26 @@
+['load', 'resize'].forEach(function(e) {
+    window.addEventListener(e, function() {
+        var body = document.body,
+        html = document.documentElement;
+        var height = Math.max(body.clientHeight, body.scrollHeight, body.offsetHeight, 
+                              html.clientHeight, html.scrollHeight, html.offsetHeight);
+        var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+        var scrollbar = document.getElementById("scrollbar");
+        if (height <= vh) { scrollbar.classList.add("off"); }
+        else { scrollbar.classList.remove("off"); }
+    });
+});
+
+var ebback = document.getElementById("ebback");
+var ebforward = document.getElementById("ebforward");
+
+if(history.length <= 1) { ebback.classList.add("off"); ebforward.classList.add("off"); }
+else { ebback.classList.remove("off"); ebforward.classList.remove("off"); }
+
+
+var ebinfo = document.getElementById("ebinfo");
+var href = ebinfo.getAttribute('href');
+
+if(!href || href.trim().length < 1) { ebinfo.classList.add("off"); }
+else { ebinfo.classList.remove("off"); }
