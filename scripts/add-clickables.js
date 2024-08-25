f460676de553dbfa83a3dd86726c768bf11379d5
laurapigeon <laurahannah44@gmail.com>
2024-08-25 11:20:01 +0100

    added initial search functionality

--- scripts/add-clickables.js
+++ scripts/add-clickables.js
@@ -7,3 +7,9 @@ const sbbottom = document.getElementById('sbbottom');
 sbbottom.addEventListener("click", function() {
     window.scrollBy(0, 64); 
 });
+
+const ebsearch = document.getElementById('ebsearch');
+const ebcontents = document.getElementById('ebcontents');
+ebsearch.addEventListener("click", function() {
+    ebcontents.classList.toggle("search");
+});
